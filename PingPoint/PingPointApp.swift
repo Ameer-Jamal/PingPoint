ba24c8fdde9bd089eebef999716be19269b0fe40
Ameer Jamal <ajamal@etq.com>
2023-11-02 09:27:26 -0700

    Initial Commit

---
+++ PingPoint/PingPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPointApp.swift
+//  PingPoint
+//
+//  Created by Ameer Jamal on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PingPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
